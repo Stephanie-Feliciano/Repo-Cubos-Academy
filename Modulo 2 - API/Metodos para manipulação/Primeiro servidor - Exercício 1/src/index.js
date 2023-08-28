57434a15b755383495344a1060d34104204765e3
Stephanie <stephaniesantanafeliciano@hotmail.com>
2023-08-28 18:52:37 -0300

    Atualização de repositório.

---
+++ Modulo 2 - API/Metodos para manipulação/Primeiro servidor - Exercício 1/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+const usuarios = [
+  { id: 11, nome: 'joao', idade: 23 },
+  { id: 2, nome: 'maria', idade: 18 },
+  { id: 4, nome: 'ana', idade: 30 },
+  { id: 1, nome: 'rodrigo', idade: 17 },
+  { id: 123, nome: 'joao', idade: 47 }
+];
+
+app.get('/usuarios', (requisição, resposta) => {
+  resposta.send(usuarios);
+})
+
+app.listen(3000, () => {
+  console.log('Servidor inicializado na porta 3000')
+});
+
+
+
