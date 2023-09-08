06dc94c48ba63dd44e9fa548bdddfea9325a3480
Stephanie <stephaniesantanafeliciano@hotmail.com>
2023-09-08 15:40:36 -0300

    Atualização de repositório

--- Modulo 2 - API/Configuração inicial do servidor/src/index.js
+++ Modulo 2 - API/Configuração inicial do servidor/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const primeiroIntermediario = (requisicao, resposta, proximo) => {
   console.log('Passei no primeiro intermediário');
-  resposta.send('Algo deu errado - isto é apenas um teste');
+  // return resposta.send('Algo deu errado - isto é apenas um teste'); // o mesmo não deixará passar para o próximo intermediário
   proximo();
 };
 
@@ -19,13 +19,19 @@ const segundoIntermediario = (requisicao, resposta, proximo) => {
   proximo();
 };
 
+const intermediarioDaRota = (requisicao, resposta, proximo) => {
+  console.log('Passei no intermediário da rota');
+  proximo();
+};
+
 app.use(primeiroIntermediario);
 app.use(segundoIntermediario);
+
 
 //Curiosidade: inserindo a URL http://localhost:3000/professores?nome=Teste2 seguido por SEND, o VSCODE irá printar a informação solicitada.
 
 // URL: localhost:3000/professores;
-app.get('/professores', filtrarProfessores);
+app.get('/professores', intermediarioDaRota, filtrarProfessores);
 
 // URL: localhost:3000/professores/3;
 app.get('/professores/:id', encontrarUmProfessor);
