916c5cef621722cdf54bf6f252bb85389f966cb5
Stephanie <stephaniesantanafeliciano@hotmail.com>
2023-09-18 14:20:31 -0300

    Atualização de repositório.

--- Modulo 2 - API/Modulo 2 API - Criando uma API REST/src/rotas.js
+++ Modulo 2 - API/Modulo 2 API - Criando uma API REST/src/rotas.js
@@ -10,6 +10,8 @@ rotas.post('/instrutores', instrutores.cadastrarInstrutor);
 rotas.put('/instrutores/:id', instrutores.atualizarInstrutor);
 rotas.patch('/instrutores/:id/status', instrutores.atualizarStatusInstrutor);
 rotas.delete('/instrutores/:id/status', instrutores.excluirInstrutor);
+
 rotas.post('/instrutores/:idInstrutor/aulas', aulas.cadastrarAulas);
+rotas.get('/aulas', aulas.listarAulas);
 
 module.exports = rotas;
